099b35448d455bdd98e09274399c2a5e1e827ccd
Kevinlzh9802 <zonghuan_li@outlook.com>
2022-07-07 12:25:24 -0700

    test

--- inclearn/models/incmodel.py
+++ inclearn/models/incmodel.py
@@ -440,10 +440,9 @@ class IncModel(IncrementalLearner):
         acc_aux = averageMeter()
         preds, targets = [], []
         preds_aux, targets_aux = [], []
-
         self._parallel_network.eval()
         with torch.no_grad():
-            for i, (inputs, lbls) in enumerate(data_loader):
+            for _, (inputs, lbls) in enumerate(data_loader):
                 inputs = inputs.to(self._device, non_blocking=True)
 
                 outputs = self._parallel_network(inputs)
@@ -464,13 +463,13 @@ class IncModel(IncrementalLearner):
 
                     aux_targets = lbls.clone()
                     targets_memory = list(set(data_loader.dataset.y))
-                    # TODO: fix this bug!
-                    for i in self._inc_dataset.targets_cur:
+                    targets_unique_sorted = sorted(list(set(self._inc_dataset.targets_cur)))
+                    for i in targets_unique_sorted:
                         targets_memory.remove(i)
                     for curr_class_i in targets_memory:
                         aux_targets[aux_targets == curr_class_i] = 0
-                    for index_i in range(len(self._inc_dataset.targets_cur)):
-                        aux_targets[aux_targets == self._inc_dataset.targets_cur[index_i]] = index_i+1
+                    for index_i in range(len(targets_unique_sorted)):
+                        aux_targets[aux_targets == targets_unique_sorted[index_i]] = index_i + 1
 
                     preds_aux.append(_preds_aux.detach().cpu().numpy())
                     targets_aux.append(aux_targets.long().cpu().numpy())
