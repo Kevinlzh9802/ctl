eff72f269d1fcabd7414cec703996464dc8eee12
srip22-ctl <srip22-ctl@svc-dc.svcl.ucsd.edu>
2022-07-14 15:38:18 -0700

    modify output

--- codes/base/scripts/train_server.sh
+++ codes/base/scripts/train_server.sh
@@ -4,9 +4,9 @@ python -m main train with "./configs/ctl1_gpu.yaml" \
         exp.ckptdir="./logs/" \
         exp.tensorboard_dir="./tensorboard/" \
         exp.debug=True \
-        trial=-1 \
-        --name="10scifar100_trial0_debug" \
-        -D \
+#        trial=-1 \
+#        --name="10scifar100_trial0_debug" \
+#        -D \
         -p \
         -c "None" \
         --force \
