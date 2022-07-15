0e238a36f22974f956b85cf8a72f37c03d8fb3a4
srip22-ctl <srip22-ctl@svc-dc.svcl.ucsd.edu>
2022-07-15 09:42:54 -0700

    modify output

--- codes/base/scripts/test_server.sh
+++ codes/base/scripts/test_server.sh
@@ -1,12 +1,12 @@
 python -m main test with "./configs/ctl1_gpu.yaml" \
-        exp.name="${name}" \
+#        exp.name="${name}" \
 #        exp.savedir="./logs/" \
 #        exp.ckptdir="./logs/" \
 #        exp.tensorboard_dir="./tensorboard/" \
 #        exp.debug=True \
 #        trial=6 \
-        --name="10scifar100_trial0_debug" \
-        -D \
-        -p \
-        -c "None" \
-        --force \
+#        --name="10scifar100_trial0_debug" \
+#        -D \
+#        -p \
+#        -c "None" \
+#        --force \
