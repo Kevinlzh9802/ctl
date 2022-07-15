53d007df3a9e02204b8a7ce65d851f3e62a0345d
srip22-ctl <srip22-ctl@svc-dc.svcl.ucsd.edu>
2022-07-15 09:59:31 -0700

    modify output

--- inclearn/models/incmodel.py
+++ inclearn/models/incmodel.py
@@ -491,6 +491,8 @@ class IncModel(IncrementalLearner):
         self.curr_acc_list = [acc]
 
         if _output_aux is not None:
+            print(output_aux)
+            print(targets_aux)
             self.record_details(output_aux, targets_aux, targets_aux, acc_aux)
             self.curr_acc_list_aux = [acc_aux]
 
