6068c5b2c2e67b036c23f2fb9d24757b67a463e8
srip22-ctl <srip22-ctl@svc-dc.svcl.ucsd.edu>
2022-07-20 17:23:31 -0700

    modify test

--- inclearn/datasets/dataset.py
+++ inclearn/datasets/dataset.py
@@ -229,25 +229,26 @@ class iCIFAR100(iCIFAR10):
 
         elif trial_i == -1:
             label_list = [['hamster', 'mouse', 'shrew', 'rabbit', 'squirrel',
-                           'man', 'boy', 'girl', 'baby', 'woman',
-                           'crab', 'snail', 'lobster', 'worm', 'spider',
-                           'dolphin', 'whale', 'otter', 'seal', 'beaver',  #
-                           'aquarium_fish', 'flatfish', 'ray', 'trout', 'shark',
-                           'motorcycle', 'bus', 'train', 'bicycle', 'pickup_truck',
-                           'palm_tree', 'willow_tree', 'maple_tree', 'oak_tree', 'pine_tree',
-                           'bridge', 'road', 'skyscraper', 'house', 'castle',
-                           'can', 'cup', 'plate', 'bowl', 'bottle',
-                           'cattle', 'camel', 'chimpanzee', 'kangaroo', 'elephant',
-                           'rose', 'tulip', 'poppy', 'orchid', 'sunflower',
-                           'forest', 'plain', 'cloud', 'mountain', 'sea',
-                           'turtle', 'crocodile', 'dinosaur', 'lizard', 'snake',  #
-                           'wardrobe', 'bed', 'couch', 'chair', 'table',
-                           'apple', 'pear', 'mushroom', 'sweet_pepper', 'orange',
-                           'bear', 'leopard', 'tiger', 'wolf', 'lion',
-                           'streetcar', 'tractor', 'tank', 'lawn_mower', 'rocket',
-                           'butterfly', 'bee', 'beetle', 'caterpillar', 'cockroach',  #
-                           'lamp', 'television', 'telephone', 'keyboard', 'clock',
-                           'raccoon', 'fox', 'porcupine', 'skunk', 'possum']]
+                           # 'man', 'boy', 'girl', 'baby', 'woman',
+                           # 'crab', 'snail', 'lobster', 'worm', 'spider',
+                           # 'dolphin', 'whale', 'otter', 'seal', 'beaver',  #
+                           # 'aquarium_fish', 'flatfish', 'ray', 'trout', 'shark',
+                           # 'motorcycle', 'bus', 'train', 'bicycle', 'pickup_truck',
+                           # 'palm_tree', 'willow_tree', 'maple_tree', 'oak_tree', 'pine_tree',
+                           # 'bridge', 'road', 'skyscraper', 'house', 'castle',
+                           # 'can', 'cup', 'plate', 'bowl', 'bottle',
+                           # 'cattle', 'camel', 'chimpanzee', 'kangaroo', 'elephant',
+                           # 'rose', 'tulip', 'poppy', 'orchid', 'sunflower',
+                           # 'forest', 'plain', 'cloud', 'mountain', 'sea',
+                           # 'turtle', 'crocodile', 'dinosaur', 'lizard', 'snake',  #
+                           # 'wardrobe', 'bed', 'couch', 'chair', 'table',
+                           # 'apple', 'pear', 'mushroom', 'sweet_pepper', 'orange',
+                           # 'bear', 'leopard', 'tiger', 'wolf', 'lion',
+                           # 'streetcar', 'tractor', 'tank', 'lawn_mower', 'rocket',
+                           # 'butterfly', 'bee', 'beetle', 'caterpillar', 'cockroach',  #
+                           # 'lamp', 'television', 'telephone', 'keyboard', 'clock',
+                           # 'raccoon', 'fox', 'porcupine', 'skunk', 'possum'
+                           ]]
             return label_list
 
     @classmethod
