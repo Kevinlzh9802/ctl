2f95feae40864d04ad565b39edaaa09342cdd598
srip22-ctl <srip22-ctl@svc-dc.svcl.ucsd.edu>
2022-07-26 12:08:31 -0700

    compare

--- inclearn/datasets/data.py
+++ inclearn/datasets/data.py
@@ -92,7 +92,8 @@ class IncrementalDataset:
     def new_task(self):
         x_train, y_train, x_val, y_val, x_test, y_test = self._get_cur_data_for_all_children()
         self.data_cur, self.targets_cur = x_train, y_train
-        self.targets_cur_unique = list(OrderedDict.fromkeys(self.targets_cur))
+        # self.targets_cur_unique = list(OrderedDict.fromkeys(self.targets_cur))
+        self.targets_cur_unique = sorted(list(set(self.targets_cur)))
         self.targets_all_unique += self.targets_cur_unique
         if self._current_task >= len(self.curriculum):
             raise Exception("No more tasks.")
